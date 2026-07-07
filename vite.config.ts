1d152d3a70a21e3d1ff0f93585dab3e2a3509f01
Daniel-Soliz <danielsoliz1973@gmail.com>
2026-07-07 14:44:57 -0300

    Add GitHub Actions workflow for deployment to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/SQUIRM.IO/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
